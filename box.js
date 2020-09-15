dcdd9f60efc3446f845a9ff836958c0810009289
utsav505 <66318744+utsav505@users.noreply.github.com>
2020-09-15 20:21:05 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,49 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':1,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("square.png");
+        World.add(world, this.body);
+        this.Visiblity= 255;
+      }
+     
+
+
+display(){
+  
+  if(this.body.speed < 3){
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    
+    imageMode(CENTER);
+    image(this.image, 0, 0, 65,65);
+    pop();
+  }
+  else{
+    push();
+
+    World.remove(world, this.body);
+    this.Visiblity = this.Visiblity - 5;
+    tint(255,this.Visiblity);
+    imageMode(CENTER);
+    image(this.image, this.body.position.x, this.body.position.y, 65, 65);
+    pop();
+  }
+  
+}
+
+
+
+
+
+
+
+        
+ 
+};
